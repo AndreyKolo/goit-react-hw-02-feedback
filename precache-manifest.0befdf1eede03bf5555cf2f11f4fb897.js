5c095d59dc832733bd24d0ba48b05637b8e5eb88
AndreyKolo <https://github.com/AndreyKolo>
2020-05-05 21:17:05 +0300

    Updates

---
+++ precache-manifest.0befdf1eede03bf5555cf2f11f4fb897.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f831433ac5c84b0b83f5cfd3ef4032",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "64f7d01f9f56ad710904",
+    "url": "/goit-react-hw-02-feedback/static/js/2.96e3bad2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.96e3bad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1fe927059b8337b7a4b",
+    "url": "/goit-react-hw-02-feedback/static/js/main.cc7f09d1.chunk.js"
+  },
+  {
+    "revision": "fc45ae581f1bd808db71",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.cc1ddb86.js"
+  }
+]);
